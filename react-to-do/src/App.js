4c4852e83082bbf5b0a765fcaf0a22683d2cc378
Trevor <trevorgillette17@gmail.com>
2018-09-20 16:31:22 -0700

    Updated to Checkpoint 1

--- react-to-do/src/App.js
+++ react-to-do/src/App.js
@@ -5,7 +5,6 @@ import ToDo from './components/ToDo.js';
 class App extends Component {
   constructor(props) {
 	  super(props);
-  }
 this.state = {
 	todos: [
 		{ description: 'Walk the cat', isCompleted: true },
@@ -13,12 +12,13 @@ this.state = {
 		{ description: 'Buy new dishes', isCompleted: false }
 	]
 };
+}
 	render() {
     return (
       <div className="App">
       <ul>
 	    { this.state.todos.map( (todo, index) =>
-		    <ToDo key={ index } description={ todo.description } isCompleted={
+		    <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
 	    )}
 	    </ul>
 	    </div>
--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'React';
+ import React, { Component } from 'react';
 
-class ToDo extends Component {
-render() {
-	return (
-		<li>
-		<input type="checkbox" checked={ this.props.IsCompleted } />
-		<span>{ this.props.description } </span>
-		</li>
-	);
-}
-}
+ class ToDo extends Component {
+   render() {
+     return 
+       <li>
+         <input type="checkbox" checked={ this.props.isCompleted } />
+         <span>{ this.props.description }</span>
+       </li>
+     );
+   }
+ }
 
-export default ToDo;
+ export default ToDo;
