0c7260b1edecd59d479df5cde8096b736fd27e17
Trevor <trevorgillette17@gmail.com>
2018-09-21 14:39:35 -0700

    Final updates done for React To-Do: Props and State

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -2,7 +2,7 @@
 
  class ToDo extends Component {
    render() {
-     return 
+     return (
        <li>
          <input type="checkbox" checked={ this.props.isCompleted } />
          <span>{ this.props.description }</span>
