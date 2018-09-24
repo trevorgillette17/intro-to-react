d7fb2dbc9804644fe8a955df241114737eed854b
Trevor <trevorgillette17@gmail.com>
2018-09-24 16:19:24 -0700

    Current to React To-Do: Events Checkpoint

--- react-to-do/src/components/ToDo.js
+++ react-to-do/src/components/ToDo.js
@@ -4,7 +4,7 @@
    render() {
      return (
        <li>
-         <input type="checkbox" checked={ this.props.isCompleted } />
+         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
          <span>{ this.props.description }</span>
        </li>
      );
